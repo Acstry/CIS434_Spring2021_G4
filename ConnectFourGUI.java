bb72f2801bf441820147a706c0a85bfbbd5c3b57
skhatri15 <skhatri15@gmail.com>
2021-04-03 21:04:37 -0400

    Added Some Gui Class code

--- ConnectFourGUI.java
+++ ConnectFourGUI.java
@@ -5,17 +5,25 @@ public class ConnectFourGUI {
         {  
         JFrame f= new JFrame("Panel Example");    
         JPanel panel=new JPanel();  
-        panel.setBounds(40,80,200,200);    
+        panel.setBounds(40,80,600,600);    
         panel.setBackground(Color.gray);  
-        JButton b1=new JButton("Button 1");     
-        b1.setBounds(50,100,80,30);    
-        b1.setBackground(Color.yellow);   
-        JButton b2=new JButton("Button 2");   
-        b2.setBounds(100,100,80,30);    
-        b2.setBackground(Color.green);   
-        panel.add(b1); panel.add(b2);  
+        JButton b1=new JButton("Row 1");     
+        b1.setBounds(50,100,80,30);     
+        JButton b2=new JButton("Row 2");   
+        b2.setBounds(100,100,80,30);     
+        JButton b3=new JButton("Row 3");
+        b3.setBounds(150,100,80,30);
+        JButton b4=new JButton("Row 4");
+        b3.setBounds(200,100,80,30);
+        JButton b5=new JButton("Row 5");
+        b3.setBounds(250,100,80,30);
+        JButton b6=new JButton("Row 6");
+        b3.setBounds(300,100,80,30);
+        JButton b7=new JButton("Row 7");
+        b3.setBounds(350,100,80,30);
+        panel.add(b1); panel.add(b2); panel.add(b3); panel.add(b4); panel.add(b5); panel.add(b6); panel.add(b7);
         f.add(panel);  
-                f.setSize(400,400);    
+                f.setSize(800,800);    
                 f.setLayout(null);    
                 f.setVisible(true);    
         }  
